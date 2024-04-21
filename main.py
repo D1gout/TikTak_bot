90d4a254a4c2e519487b3ede7be9e6207b7a2704
D1gout <fanolkon@gmail.com>
2024-04-21 07:45:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import TOKEN, db
